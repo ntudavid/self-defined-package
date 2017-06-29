959f638827e58b2be875d1b8018d7adc5b9e0ef5
ntudavid <weiwenhsudavid@gmail.com>
2017-06-29 16:21:52 -0400

    add functions

--- func.py
+++ func.py
@@ -427,8 +427,49 @@ def primeFactorize(num):
         if(num==1):
             break
     return factors
-    
 
+def nchoosek(n,k):
+    if(n<k):
+        return 0
+    elif(k==0):
+        return 1
+    else: # n>=k
+        #lookup = np.zeros((n+1,k+1),np.int64)
+        lookup = []
+        for i in range(n+1):
+            lookup.append([0]*(k+1))
+        # initial conditions
+        for i in range(n+1):
+            lookup[i][0] = 1
+            lookup[i][1] = i
+        for j in range(k+1):
+            lookup[j][j] = 1
+        # complete the talbe
+        for i in range(1,n+1):
+            for j in range(1,k+1):
+                if(i>j):
+                    lookup[i][j] = lookup[i-1][j] + lookup[i-1][j-1]
+                else:
+                    break
+        #print(lookup)
+        return lookup[n][k]
+
+def fibonacci(n):
+    f0 = 0
+    f1 = 1
+    if(n==0): # F(0)
+        fib = f0
+    elif(n==1): # F(1)
+        fib = f1
+    else: # F(2)...
+        fib = f0+f1
+        cnt = 2
+        while(cnt<n):
+            f0 = f1
+            f1 = fib
+            fib = f0+f1
+            cnt += 1
+    return fib
 
 
 
